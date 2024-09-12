6b50c4c654ff15880ca1f4a80563a1b280edea50
UoOsSe <143217955+ousey-ousey@users.noreply.github.com>
2024-09-12 12:48:26 +0300

    ++

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <div>
       <h3 className="text-3xl font-bold text-red-800 underline">
-        Footer Component!!
+        Footer Component!! ++
         <BsGearWideConnected />
       </h3>
     </div>
