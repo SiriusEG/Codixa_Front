a1a85eefd9d1e543172bd5a122f6a8eae8dfffad
UoOsSe <143217955+ousey-ousey@users.noreply.github.com>
2024-09-12 12:04:49 +0300

    tailwind addjust

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/**/*.{js,jsx,ts,tsx}",
-    "./src/component/**/*.{js,jsx,ts,tsx}",
-    "./src/**/*.{js,jsx,ts,tsx}", // This is generally sufficient if the above paths are already included
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
