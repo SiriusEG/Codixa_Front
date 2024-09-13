48816b5fb6d96ec34feb4651bc15e944ec8bea92
UoOsSe <143217955+ousey-ousey@users.noreply.github.com>
2024-09-13 08:07:59 +0300

    add react slick library and post blog component

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -1,11 +1,12 @@
-
-import Slider from "@/component/Slider";
+import PostCSblogList from "@/component/PostCS";
+import SliderCOM from "@/component/SliderCS";
 import React from "react";
 
 export default function page() {
   return (
     <div>
-      <Slider />
+      <SliderCOM />
+      <PostCSblogList />
     </div>
   );
-};
+}
