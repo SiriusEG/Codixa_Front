8fa0a1537d50bd3ee59a8a1ea815e194fe17aed4
yasser-sayed <yassersayed952@gmail.com>
2024-09-17 03:13:05 -0700

    some changes in header and courses page

--- src/app/(pages)/components/header/Header.jsx
+++ src/app/(pages)/components/header/Header.jsx
@@ -8,14 +8,13 @@ import Search from "./Search";
 export default function Header() {
   return (
     <header className="bg-white">
-      <div className=" flex h-auto w-full items-center gap-x-1 sm:gap-x-6 px-4 py-2 sm:px-6 lg:px-8 flex-wrap">
+      <div className=" flex h-auto lg:max-w-fit w-full items-center gap-x-1 sm:gap-x-6 px-4 py-2 sm:px-6 lg:px-8 flex-wrap mx-auto">
         {/* logo */}
         <Link className=" text-primary mr-auto" href="/">
-          <span className="sr-only">Home</span>
           <Image src="/logo.gif" alt="logo" width={50} height={50} />
         </Link>
 
-        <div className="hidden lg:flex flex-1 justify-around gap-4 items-center ">
+        <div className="hidden lg:flex flex-1 justify-center gap-6 items-center ">
           {/* search bar */}
           <Search />
 
@@ -24,7 +23,7 @@ export default function Header() {
         </div>
 
         {/* login and signup */}
-        <div className="flex items-center gap-1 sm:gap-4">
+        <div className="flex items-center gap-2 sm:gap-4">
           <Link
             className=" rounded-md text-sm bg-gray-100 px-3  py-2.5  font-semibold text-primary transition hover:text-hoverPrimary "
             href="/registration"
