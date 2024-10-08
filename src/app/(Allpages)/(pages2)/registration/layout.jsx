9d3256aa3daf1e52f65befa44194808875270816
UoOsSe <143217955+ousey-ousey@users.noreply.github.com>
2024-10-08 10:12:04 +0300

    tiny fix #

--- src/app/(Allpages)/(pages2)/registration/layout.jsx
+++ src/app/(Allpages)/(pages2)/registration/layout.jsx
@@ -5,37 +5,39 @@ import Link from "next/link";
 
 const layout = ({ children }) => {
   return (
-    <div className="  flex flex-col items-center justify-center  mt-0 bg-white sm:bg-transparent px-4 py-3 sm:p-1 z-10 sm:z-0 rounded-3xl sm:rounded-none ">
-      {/* upper area  */}
-      <div className=" relative flex w-full sm:w-96 flex-col items-center justify-center gap-6">
-        <h4 className="text-xl text-center">
-          Welcome to{" "}
-          <span className="text-2xl font-bold text-center text-primary">
-            Codixa
-          </span>
-        </h4>
-        <Link
-          href="/"
-          type="button"
-          className="flex absolute left-4 sm:left-[-6rem] md:left-[-6rem] top-[-5rem] items-center gap-2 self-start mx-4 text-base font-medium transition duration-300 hover:text-primary hover:bg-primary/25 py-2 px-4 rounded-2xl"
-        >
-          <FaArrowLeft />
-          Back To Home
-        </Link>
+    <div className="relative">
+      <div className="  flex flex-col items-center justify-center  mt-0 bg-white sm:bg-transparent px-4 py-3 sm:p-1 z-10 sm:z-0 rounded-3xl sm:rounded-none ">
+        {/* upper area  */}
+        <div className="  flex w-full sm:w-96 flex-col items-center justify-center gap-6">
+          <h4 className="text-xl text-center">
+            Welcome to{" "}
+            <span className="text-2xl font-bold text-center text-primary">
+              Codixa
+            </span>
+          </h4>
+          <Link
+            href="/"
+            type="button"
+            className="flex absolute left-4 sm:left-[-6rem] md:left-[-10rem] top-[-3%] items-center gap-2 self-start mx-4 text-base font-medium transition duration-300 hover:text-primary hover:bg-primary/25 py-2 px-4 rounded-2xl"
+          >
+            <FaArrowLeft />
+            Back To Home
+          </Link>
 
-        <Switch />
-        <p
-          style={{ marginLeft: "-15px", textAlign: "justify" }}
-          className=" font-[300] text-left mt-10 w-[80%]  text-[.7rem] text-secondary"
-        >
-          {" "}
-          Continuous learning through micro-courses or certifications keeps
-          skills relevant, driving talent development in rapidly evolving
-          industries.{" "}
-        </p>
-        {/* Form area */}
-        <div className="w-full max-w-sm transition-all duration-700 ease-out transform">
-          {children}
+          <Switch />
+          <p
+            style={{ marginLeft: "-15px", textAlign: "justify" }}
+            className=" font-[300] text-left mt-10 w-[80%]  text-[.7rem] text-secondary"
+          >
+            {" "}
+            Continuous learning through micro-courses or certifications keeps
+            skills relevant, driving talent development in rapidly evolving
+            industries.{" "}
+          </p>
+          {/* Form area */}
+          <div className="w-full max-w-sm transition-all duration-700 ease-out transform">
+            {children}
+          </div>
         </div>
       </div>
     </div>
