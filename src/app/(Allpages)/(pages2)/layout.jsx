8268b3bac549144227e3137f7a3827010e2557d1
yasser-sayed <yassersayed952@gmail.com>
2024-10-09 03:03:55 -0700

    cleaning the layouts, fix the authCheck and headerLoading, adding framer-motion lib, adding user dropMenu

--- src/app/(Allpages)/(pages2)/layout.jsx
+++ src/app/(Allpages)/(pages2)/layout.jsx
@@ -1,6 +1,4 @@
-import { Inter } from "next/font/google";
 import "./global.css";
-import StoreProvider from "../../StoreProvider";
 import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
 import Image from "next/image";
 
@@ -12,43 +10,36 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className="overflow-x-hidden font-primary">
-        <StoreProvider>
-          <div className="w-full">
-            <div className="flex h-screen items-center sm:flex-row">
-              {/* image side */}
-              <div className="sm:w-1/2 w-full relative hidden sm:block">
-                <Image
-                  src="/learnmore.jpg"
-                  alt="teacher"
-                  width={800}
-                  height={500}
-                  className="h-2/5 w-full sm:h-screen"
-                />
-                <div className="absolute inset-0 bg-black opacity-30" />
-                <div className="absolute bottom-1/2 left-5">
-                  <h1 className="text-6xl text-white font-bold text-left pt-10">
-                    <span className="text-7xl text-primary">C</span>odixa
-                  </h1>
-                  <p className="text-white backdrop-blur-sm text-center text-xl font-semibold p-[25px] rounded-md relative right-4">
-                    <FaQuoteLeft className="text-black mb-2" />
-                    Learning is not a destination, but a continuous journey. The
-                    more you seek to improve, the further you discover you can
-                    go. Growth happens when you embrace the process, not just
-                    the goal.
-                    <FaQuoteRight className="text-black right-0 absolute" />
-                  </p>
-                </div>
-              </div>
-              {/* children side */}
-              <div className="sm:w-1/2 w-full flex items-center justify-center">
-                {children}
-              </div>
-            </div>
+    <div className="w-full">
+      <div className="flex h-screen items-center sm:flex-row">
+        {/* image side */}
+        <div className="sm:w-1/2 w-full relative hidden sm:block">
+          <Image
+            src="/learnmore.jpg"
+            alt="teacher"
+            width={800}
+            height={500}
+            className="h-2/5 w-full sm:h-screen"
+          />
+          <div className="absolute inset-0 bg-black opacity-30" />
+          <div className="absolute bottom-1/2 left-5">
+            <h1 className="text-6xl text-white font-bold text-left pt-10">
+              <span className="text-7xl text-primary">C</span>odixa
+            </h1>
+            <p className="text-white backdrop-blur-sm text-center text-xl font-semibold p-[25px] rounded-md relative right-4">
+              <FaQuoteLeft className="text-black mb-2" />
+              Learning is not a destination, but a continuous journey. The more
+              you seek to improve, the further you discover you can go. Growth
+              happens when you embrace the process, not just the goal.
+              <FaQuoteRight className="text-black right-0 absolute" />
+            </p>
           </div>
-        </StoreProvider>
-      </body>
-    </html>
+        </div>
+        {/* children side */}
+        <div className="sm:w-1/2 w-full flex items-center justify-center">
+          {children}
+        </div>
+      </div>
+    </div>
   );
 }
